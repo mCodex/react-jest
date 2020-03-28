859661b3349b5ad5254c635848e2afcac7ef10f6
Mateus Andrade <mat.andrade@live.com>
2020-03-28 12:19:32 -0300

    added simple tests using localStorage mock

--- src/components/TechList/__tests__/index.js
+++ src/components/TechList/__tests__/index.js
@@ -1,16 +1,45 @@
 import React from 'react';
-import { render, fireEvent } from '@testing-library/react';
+import { render, fireEvent, cleanup } from '@testing-library/react';
 
 import TechList from '../index';
 
 describe('TechList Component', () => {
+    beforeEach(() => {
+        localStorage.clear();
+    });
+
     it('should be able to add new tech', () => {
-        const { getByText, getByTestId, debug } = render(<TechList />);
+        const { getByText, getByTestId, getByLabelText } = render(<TechList />);
         // debug();
 
-        fireEvent.click(getByText('Adicionar'));
+        fireEvent.change(getByLabelText('Tech'), {
+            target: { value: 'Node.js' },
+        });
 
+        fireEvent.submit(getByTestId('tech-form'));
         // debug();
+
+        expect(getByTestId('tech-list')).toContainElement(getByText('Node.js'));
+        expect(getByLabelText('Tech')).toHaveValue('');
+    });
+
+    it('should store techs in storage', () => {
+        let { getByText, getByTestId, getByLabelText } = render(<TechList />);
+
+        fireEvent.change(getByLabelText('Tech'), {
+            target: { value: 'Node.js' },
+        });
+
+        fireEvent.submit(getByTestId('tech-form'));
+
+        cleanup();
+
+        ({ getByTestId, getByLabelText, getByText } = render(<TechList />));
+
+        expect(localStorage.setItem).toHaveBeenCalledWith(
+            'techs',
+            JSON.stringify(['Node.js'])
+        );
 
         expect(getByTestId('tech-list')).toContainElement(getByText('Node.js'));
     });
--- src/components/TechList/index.js
+++ src/components/TechList/index.js
@@ -1,21 +1,44 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 export default () => {
     const [techs, setTechs] = useState([]);
+    const [newTech, setNewTech] = useState('');
 
-    const handleAddTech = () => setTechs([...techs, 'Node.js']);
+    useEffect(() => {
+        const techsFromStorage = localStorage.getItem('techs');
+
+        if (techsFromStorage) {
+            setTechs(JSON.parse(techsFromStorage));
+        }
+    }, []);
+
+    useEffect(() => {
+        localStorage.setItem('techs', JSON.stringify(techs));
+    }, [techs]);
+
+    const handleAddTech = () => {
+        setTechs([...techs, newTech]);
+        setNewTech('');
+    };
 
     return (
-        <div>
+        <form data-testid="tech-form" onSubmit={handleAddTech}>
             <ul data-testid="tech-list">
                 {techs.map(tech => (
                     <li key={tech}>{tech}</li>
                 ))}
             </ul>
 
+            <label htmlFor="tech">Tech</label>
+            <input
+                id="tech"
+                value={newTech}
+                onChange={e => setNewTech(e.target.value)}
+            />
+
             <button type="button" onClick={handleAddTech}>
                 Adicionar
             </button>
-        </div>
+        </form>
     );
 };
